19bff731757935822817fbff50b38079cabb3367
Yunyan <708931190@qq.com>
2022-08-06 17:30:01 +0800

    add: 左侧菜单树开发

--- .umirc.ts
+++ .umirc.ts
@@ -1,3 +1,18 @@
+/**
+ * @Author: fanx
+ * @Date: 2022年08月06日 11:41
+ * @Description: umi项目的配置文件
+ */
 export default {
-  npmClient: 'pnpm'
+  npmClient: 'pnpm',
+  // 页面标题
+  title: 'my umi project',
+  // 路由路径，当前端项目没有部署在nginx的根目录时，必须修改这个路径
+  base: '/',
+  // 构建时压缩css
+  cssMinifier: 'esbuild',
+  // 检测未使用的文件和导出，仅在 build 阶段开启。
+  deadCode: {},
+  // 配置额外的css，作为style标签插入index.html中
+  styles: [`html,body { height: 100%; margin: 0; }`, `#root { height: 100%; }`],
 };
--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,21 +1,154 @@
-import { Link, Outlet } from 'umi';
-import styles           from './index.less';
+import { Menu } from '@arco-design/web-react';
+import '@arco-design/web-react/dist/css/arco.css';
+import { IconApps, IconBug, IconBulb } from '@arco-design/web-react/icon';
+import styled from '@emotion/styled';
+import { positionValues, Scrollbars } from 'react-custom-scrollbars';
+import apple from '../assets/apple.svg';
+import './base.css';
+
+const MenuItem = Menu.Item;
+const SubMenu = Menu.SubMenu;
+
+export const Container = styled.div({
+  height: '100%',
+  backgroundColor: '#FFFFFF',
+});
+
+const NavigationBar = styled.div({
+  width: 300,
+  height: '100%',
+  padding: '20px 0',
+  borderRight: '2px solid #F6F6F6',
+  boxSizing: 'border-box',
+  overflow: 'auto',
+});
+
+const HeaderLogo = styled.div({
+  display: 'flex',
+  height: 50,
+  justifyContent: 'center',
+  marginBottom: 10,
+});
+
+const logo_text_style = {
+  fontWeight: 'bolder',
+  color: '#F5414A',
+  fontSize: 25,
+  verticalAlign: '12px',
+  letterSpacing: '5px',
+};
+
+export const onScrollFrame = (values: positionValues) => {
+  console.log(values);
+};
 
 export default function Layout() {
   return (
-    <div className={styles.navs}>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/docs">Docs</Link>
-        </li>
-        <li>
-          <a href="https://github.com/umijs/umi">Github</a>
-        </li>
-      </ul>
-      <Outlet />
-    </div>
+    <Container>
+      <NavigationBar>
+        <Scrollbars
+          autoHide
+          autoHideTimeout={1000}
+          onScrollFrame={onScrollFrame}
+        >
+          <HeaderLogo>
+            <div
+              style={{
+                display: 'inline-block',
+                fontSize: 0,
+              }}
+            >
+              <img src={apple} alt="lemon-logo" width={50} height={50} />
+              <div
+                style={{ width: 10, display: 'inline-block', fontSize: 0 }}
+              />
+              <span style={logo_text_style}>AMAZING</span>
+            </div>
+          </HeaderLogo>
+
+          <Menu>
+            <SubMenu
+              key="0"
+              title={
+                <>
+                  <IconApps /> Navigation 1
+                </>
+              }
+            >
+              <MenuItem key="0_0">Menu 1</MenuItem>
+              <MenuItem key="0_1">Menu 2</MenuItem>
+              <MenuItem key="0_2" disabled>
+                Menu 3
+              </MenuItem>
+            </SubMenu>
+            <SubMenu
+              key="1"
+              title={
+                <>
+                  <IconBug /> Navigation 2
+                </>
+              }
+            >
+              <MenuItem key="1_0">Menu 1</MenuItem>
+              <MenuItem key="1_1">Menu 2</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+              <MenuItem key="1_2">Menu 3</MenuItem>
+            </SubMenu>
+            <SubMenu
+              key="2"
+              title={
+                <>
+                  <IconBulb /> Navigation 3
+                </>
+              }
+            >
+              <MenuItem key="2_0">Menu 1</MenuItem>
+              <MenuItem key="2_1">Menu 2</MenuItem>
+              <MenuItem key="2_2">Menu 3</MenuItem>
+            </SubMenu>
+          </Menu>
+        </Scrollbars>
+      </NavigationBar>
+    </Container>
   );
 }
